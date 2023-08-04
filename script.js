3ea9e848440ec54af3293a9f07f74be2763921b6
Guillaume Gonnet <111474584+guillaume-gonnet@users.noreply.github.com>
2023-08-03 23:41:45 -0400

    rewrite updateBuffer, working for operate

--- script.js
+++ script.js
@@ -1,50 +1,82 @@
-const screen = document.getElementById("screen");
-const operatorsUI = document.getElementById("operators");
-const numbersUI = document.getElementById("numbers");
-
 let buffer = [];
-const operators = ["+", "-", "*", "/"];
+const operators = ["+", "-", "*", "/", "="];
 
-//create numbers button
-for (let i = 0; i < 10; i++) {
-    let button = document.createElement("button");
-    button.innerText = i;
-    button.addEventListener('click', (e) => {
-        e.stopPropagation();
-        updateBuffer(i);
-    });
-    numbersUI.appendChild(button);
+createButtons();
+
+function createButtons() {
+    const operatorsUI = document.getElementById("operators");
+    for (let i = 0; i < 5; i++) {
+        let button = document.createElement("button");
+        button.innerText = operators[i];
+        button.addEventListener('click', (e) => {
+            e.stopPropagation();
+            buffer = updateBuffer(buffer, operators[i]);
+        });
+        operatorsUI.appendChild(button);
+    }
+
+
+    const numbersUI = document.getElementById("numbers");
+    for (let i = 0; i < 10; i++) {
+        let button = document.createElement("button");
+        button.innerText = i;
+        button.addEventListener('click', (e) => {
+            e.stopPropagation();
+            buffer = updateBuffer(buffer, i);
+        });
+        numbersUI.appendChild(button);
+    }
 }
 
 function updateScreen(num) {
+    const screen = document.getElementById("screen");
     screen.innerText = num;
 
 }
 
-function updateBuffer(el) {
-    let lastEl = buffer.at(-1) || 0;
+function updateBuffer(buff, el) {
 
-    if (buffer.length === 0) {
-        buffer.push(el);
-        updateScreen(buffer[0]);
-        return;
-    }
-    if (buffer.length === 1 && typeof buffer[0] === "number") {
-        buffer.splice(-1, 1, lastEl * 10 + el);
-        updateScreen(buffer[0]);
-        return;
+    if (buff.length === 0) {
+        if (typeof el === "number") {
+            buff.push(el);
+            updateScreen(buff[0]);
+        }
+        return buff;
     }
 
-
-    if (operators.findIndex((e) => e === lastEl) !== -1) {
-        executeOperation();
+    if (typeof el === "number") {
+        if (typeof buff.at(-1) === "number") {
+            buff.splice(-1, 1, buff.at(-1) * 10 + el);
+            updateScreen(buff.at(-1));
+            return buff;
+        } else {
+            buff.push(el);
+            updateScreen(buff.at(-1));
+            return buff;
+        }
+    } else if (el === "=") {
+        buff.push(operate(buff.pop(), buff.pop(), buff.pop()));
+        updateScreen(buff.at(-1));
+        return buff;
     } else {
-        const newEl = lastEl * 10 + el;
-        buffer.splice(-2, 2, newEl);
-        updateBuffer(newEl);
+        buff.push(el);
+        return buff;
     }
 }
 
-function executeOperation() {
-    //TODO
+
+// a is actually the second argument, and b the first
+function operate(a, ope, b) {
+    switch (ope) {
+        case "+":
+            return a + b;
+        case "-":
+            return b - a;
+        case "*":
+            return a * b;
+        case "/":
+            return b / a
+    }
 }
+
+// module.exports = updateBuffer;
