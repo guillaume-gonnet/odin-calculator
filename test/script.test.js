7614c7dbf3eb0a004cd13eeb59577d7186f147d9
Guillaume Gonnet <111474584+guillaume-gonnet@users.noreply.github.com>
2023-08-04 22:47:54 -0400

    add tests

--- test/script.test.js
+++ test/script.test.js
@@ -158,3 +158,41 @@ test('Addition / Multiplication order - Multiple', () => {
     buffer = updateBuffer(buffer, "=");
     expect(buffer).toStrictEqual([441.6666666666667]);
 });
+
+test('= after only 1 element', () => {
+
+    document.body.innerHTML =
+        '< container > ' +
+        '<div id="screen"></div>' +
+        '<div id="operators"></div>' +
+        '<div id="numbers"></div>' +
+        '</container>';
+
+    const updateBuffer = require('../script');
+
+    let buffer = [];
+    buffer = updateBuffer(buffer, 5);
+    buffer = updateBuffer(buffer, 2);
+    buffer = updateBuffer(buffer, "=");
+    expect(buffer).toStrictEqual([52]);
+});
+
+test('Div by 0 should not error', () => {
+
+    document.body.innerHTML =
+        '< container > ' +
+        '<div id="screen"></div>' +
+        '<div id="operators"></div>' +
+        '<div id="numbers"></div>' +
+        '</container>';
+
+    const updateBuffer = require('../script');
+
+    let buffer = [];
+    buffer = updateBuffer(buffer, 5);
+    buffer = updateBuffer(buffer, 2);
+    buffer = updateBuffer(buffer, "/");
+    buffer = updateBuffer(buffer, 0);
+    buffer = updateBuffer(buffer, "=");
+    expect(buffer).toStrictEqual(["nan"]);
+});
